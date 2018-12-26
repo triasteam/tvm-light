70f522ea5bf58b845504151b9108cf7d778aa534
wangjunqing <wunder3605@126.com>
2018-12-26 19:13:21 +0800

    update python-script part

--- python-script/app.py
+++ python-script/app.py
@@ -1,23 +1,28 @@
 import ConfigParser
 import time
 import json
+import hashlib
 import ipfsapi
+import requests
 from flask import Flask, request
 from iota_cache import IotaCache
 from tag_generator import TagGenerator
 
-import requests
 
 app = Flask(__name__)
 
 cf = ConfigParser.ConfigParser()
 cf.read("conf")
+
 iota_addr = cf.get("iota", "addr")
 iota_seed = cf.get("iota", "seed")
 cache = IotaCache(iota_addr, iota_seed)
+
 ipfs_ip = cf.get("ipfs", "ip")
 ipfs_port = cf.get("ipfs", "port")
 ipfs_client = ipfsapi.connect(ipfs_ip, ipfs_port)
+
+tvm_addr = cf.get("tvm", "addr")
 
 
 @app.route('/')
@@ -51,8 +56,7 @@ def upload_contract():
     #     return 'request error'
     # hash_list = []
     # for file_key in request.files:
-    #     result = ipfs_client.block_put(request.files[file_key])
-    #     ipfs_hash = result["Key"]
+    #     ipfs_hash = ipfs_client.add_str(request.files[file_key].read())
     #     if ipfs_hash[:2] != "Qm":
     #         return 'ipfs error'
     #     hash_list.append(ipfs_hash)
@@ -60,52 +64,164 @@ def upload_contract():
     # for hash in hash_list:
     #     print hash
 
-    result_dict = {}
+    result_dict = dict()
+    result_dict['type'] = 'contract'
     contract_file = request.files.get('contract', None)
     if contract_file is None:
         return 'no contract'
+    ipfs_hash = ipfs_client.add_str(contract_file.read())
 
-    ipfs_result = ipfs_client.block_put(contract_file)
-    ipfs_hash = ipfs_result['Key']
     if ipfs_hash[:2] != 'Qm':
         return 'ipfs error'
     result_dict['address'] = ipfs_hash
 
-    command = request.form.get("command", None)
-    if command is None:
-        return 'no command'
-    result_dict['command'] = command
+    md5 = request.form.get('md5', None)
+    if md5 is None:
+        return 'no md5'
+    result_dict['checkMD5'] = md5
 
-    result_dict['contractName'] = 'contract_' + str(time.time())
+    result_dict['command'] = ''
+    result_dict['contractName'] = 'contract_' + str(int(round(time.time())))
     result_dict['contractType'] = 'fabric'
     result_dict["contractVersion"] = "v1.0"
     result_dict['vmVersion'] = '1.0'
-    result_dict['sequence'] = '10'
-    result_dict['timestamp'] = str(time.time())
+    result_dict['sequence'] = '0'
+    result_dict['timestamp'] = int(round(time.time()))
     result_dict['user'] = 'user1'
     result_dict['signature'] = ''
     result_dict['operation'] = 'install'
 
-    # temporary post data to tvm
-    r = requests.post('http://192.168.199.129:8080/executeContract', json=json.dumps(result_dict))
-    if r.status_code == 200:
-        return 'ok'
-    else:
-        return 'error'
+    print result_dict
 
-    # # cache in tangle part
-    #
-    # result_flag, result_msg = cache_json_in_tangle(result_dict)
-    #
-    # if result_flag is True:
-    #     return 'ok'
-    # else:
-    #     return result_msg
+    # cache in tangle part
+    result_flag, result_msg = cache_json_in_tangle(result_dict)
+
+    if result_flag is True:
+        return "".join([
+            "SUCCESS \n\n",
+            "[INIT-CMD]:\n",
+            "curl -X POST \\ \n",
+            "  http://127.0.0.1:5000/upload_action \\ \n",
+            "  -H 'cache-control: no-cache' \\ \n",
+            "  -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \\ \n",
+            "  -F md5=", result_dict['checkMD5'], " \\ \n",
+            "  -F contractName=", result_dict['contractName'], " \\ \n",
+            """  -F 'command={"Args":["init","a","7","b","8"]}' \\ \n""",
+            "  -F operation=instantiate \\ \n",
+            "  -F address=", result_dict['address'], " \n"
+        ])
+    else:
+        return result_msg
 
 
 @app.route('/upload_action', methods=['POST'])
 def upload_action():
-    pass
+
+    result_dict = dict()
+    result_dict['type'] = 'contract'
+
+    address = request.form.get('address', None)
+    if address is None:
+        return 'no address'
+    result_dict['address'] = address
+
+    md5 = request.form.get('md5', None)
+    if md5 is None:
+        return 'no md5'
+    result_dict['checkMD5'] = md5
+
+    contract_name = request.form.get("contractName", None)
+    if contract_name is None:
+        return 'no contractName'
+    result_dict['contractName'] = contract_name
+
+    command = request.form.get("command", None)
+    if command is None:
+        return 'no command'
+    result_dict['command'] = json.dumps(json.loads(command))
+
+    result_dict['contractType'] = 'fabric'
+    result_dict["contractVersion"] = "v1.0"
+    result_dict['vmVersion'] = '1.0'
+    result_dict['sequence'] = '0'
+    result_dict['timestamp'] = int(round(time.time()))
+    result_dict['user'] = 'user1'
+    result_dict['signature'] = ''
+    operation = request.form.get('operation', None)
+    if operation is None:
+        return 'no operation'
+    elif operation not in ['instantiate', 'query']:
+        return 'wrong operation'
+    result_dict['operation'] = operation
+
+    print result_dict
+
+    # cache in tangle part
+    result_flag, result_msg = cache_json_in_tangle(result_dict)
+
+    if result_flag is True:
+        return "".join([
+            "SUCCESS \n\n",
+            "[QUERY-CMD]:\n",
+            "curl -X POST \\ \n",
+            "  http://127.0.0.1:5000/query_action \\ \n",
+            "  -H 'cache-control: no-cache' \\ \n",
+            "  -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \\ \n",
+            "  -F md5=", result_dict['checkMD5'], " \\ \n",
+            "  -F contractName=", result_dict['contractName'], " \\ \n",
+            """  -F 'command={"Args":["query","a"]}' \\ \n""",
+            "  -F address=", result_dict['address'], " \n"
+        ])
+    else:
+        return result_msg
+
+
+@app.route('/query_action', methods=['POST'])
+def query_action():
+
+    result_dict = dict()
+    result_dict['type'] = 'contract'
+
+    address = request.form.get('address', None)
+    if address is None:
+        return 'no address'
+    result_dict['address'] = address
+
+    md5 = request.form.get('md5', None)
+    if md5 is None:
+        return 'no md5'
+    result_dict['checkMD5'] = md5
+
+    contract_name = request.form.get("contractName", None)
+    if contract_name is None:
+        return 'no contractName'
+    result_dict['contractName'] = contract_name
+
+    command = request.form.get("command", None)
+    if command is None:
+        return 'no command'
+    result_dict['command'] = json.dumps(json.loads(command))
+
+    result_dict['contractType'] = 'fabric'
+    result_dict["contractVersion"] = "v1.0"
+    result_dict['vmVersion'] = '1.0'
+    result_dict['sequence'] = '0'
+    result_dict['timestamp'] = int(round(time.time()))
+    result_dict['user'] = 'user1'
+    result_dict['signature'] = ''
+    result_dict['operation'] = 'query'
+
+    print result_dict
+
+    headers = {
+        'Content-Type': "application/json",
+        'cache-control': "no-cache"
+    }
+    result = requests.get(tvm_addr + '/executeContract', data=json.dumps(result_dict), headers=headers)
+    if result.status_code == 200:
+        return result.content
+    else:
+        return "tvm error"
 
 
 def cache_json_in_tangle(result_json):
@@ -115,7 +231,7 @@ def cache_json_in_tangle(result_json):
     if ipfs_hash[:2] == "Qm":
         print("[INFO]Cache json %s in ipfs, the hash is %s." % (json.dumps(result_json, sort_keys=True), ipfs_hash))
 
-        cache.cache_txn_in_tangle(ipfs_hash, TagGenerator.get_current_tag())
+        cache.cache_txn_in_tangle_sdk(ipfs_hash, TagGenerator.get_current_tag())
         print("[INFO]Cache hash %s in tangle, the tangle tag is %s." % (ipfs_hash, TagGenerator.get_current_tag()))
 
         return True, 'ok'
@@ -123,6 +239,13 @@ def cache_json_in_tangle(result_json):
     else:
         print("[ERROR]Cache ipfs error, error message is %s." % ipfs_hash)
         return False, 'ipfs error'
+
+
+def get_md5(file_content):
+
+    hash_md5 = hashlib.md5(file_content)
+
+    return hash_md5.hexdigest()
 
 
 if __name__ == '__main__':
